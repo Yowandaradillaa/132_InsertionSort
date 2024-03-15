4f566d3c36c0bf314bd155b0d00dce656056a62c
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-03-15 10:58:59 +0700

    Memasukkan Void input

--- PertemuanKeempatPrakAlgo/PertemuanKeempatPrakAlgo.cpp
+++ PertemuanKeempatPrakAlgo/PertemuanKeempatPrakAlgo.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 using namespace std;
 
+int n;
+int arr[11];
 
-void insertionSort() {
-	int i, n, temp, Ij;
-	int arr[11];
+void input() {
+	while (true) {
+		cout << "Masuka banyak elemen pada array : ";
+		cin >> n;
+		if (n <= 20)
+			break;
+		else {
+			cout << "\nArray dapat mempunyai maksimal 20 elemen.\n";
+		}
+	}
+	cout << endl;
+	cout << "===================" << endl;
+	cout << "Masukan Elemen Array" << endl;
+	cout << "===================" << endl;
 
+	for (int i = 0; i < n; i++) {
+		cout << "Data ke-" << (i + 1) << ": ";
+		cin >> arr[i];
+	}
 }
 
-
-int main() 
-{
 
 }
 
